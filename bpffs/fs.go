dfdb444722c666ea58682c5dd3d620a697df2d36
Alfonso Acosta <fons@weave.works>
2017-05-26 02:21:32 +0000

    Use an unsafe cast instead

--- bpffs/fs.go
+++ bpffs/fs.go
@@ -3,11 +3,20 @@ package bpffs
 import (
 	"fmt"
 	"syscall"
+	"unsafe"
 )
 
-const (
-	BPFFSPath = "/sys/fs/bpf"
-)
+const BPFFSPath = "/sys/fs/bpf"
+
+var FsMagicBPFFS int32
+
+func init() {
+	// https://github.com/coreutils/coreutils/blob/v8.27/src/stat.c#L275
+	magic := uint32(0xCAFE4A11)
+	// 0xCAFE4A11 overflows an int32, which is what's expected by Statfs_t.Type in 32bit platforms.
+	// To avoid conditional compilation for all 32bit/64bit platforms, we use an unsafe cast
+	FsMagicBPFFS = *(*int32)(unsafe.Pointer(&magic))
+}
 
 // IsMounted checks if the BPF fs is mounted already
 func IsMounted() (bool, error) {
@@ -15,8 +24,7 @@ func IsMounted() (bool, error) {
 	if err := syscall.Statfs(BPFFSPath, &data); err != nil {
 		return false, fmt.Errorf("cannot statfs %q: %v", BPFFSPath, err)
 	}
-	// https://github.com/coreutils/coreutils/blob/v8.27/src/stat.c#L275
-	return data.Type == 0xCAFE4A11, nil
+	return int32(data.Type) == FsMagicBPFFS, nil
 }
 
 // Mount mounts the BPF fs if not already mounted
