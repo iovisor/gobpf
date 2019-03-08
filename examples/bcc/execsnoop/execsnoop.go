d70389c0e330a5b515b4009a222d8428e20aaebe
Josh Chorlton <choo@stripe.com>
2019-03-08 15:59:44 -0800

    Support maxActive params when attaching Kprobes and Kretprobes
    Passing anything <= 0 uses the default value
    The kernel docs for the parameter are here: https://www.kernel.org/doc/Documentation/kprobes.txt
    See https://github.com/iovisor/bcc/commit/1d6d45cd2604af307c5f0e502df57fe66fd869d5

--- examples/bcc/execsnoop/execsnoop.go
+++ examples/bcc/execsnoop/execsnoop.go
@@ -204,7 +204,9 @@ func run() {
 		os.Exit(1)
 	}
 
-	if err := m.AttachKprobe(fnName, kprobe); err != nil {
+	// passing -1 for maxActive signifies to use the default
+	// according to the kernel kprobes documentation
+	if err := m.AttachKprobe(fnName, kprobe, -1); err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to attach syscall__execve: %s\n", err)
 		os.Exit(1)
 	}
@@ -215,7 +217,9 @@ func run() {
 		os.Exit(1)
 	}
 
-	if err := m.AttachKretprobe(fnName, kretprobe); err != nil {
+	// passing -1 for maxActive signifies to use the default
+	// according to the kernel kretprobes documentation
+	if err := m.AttachKretprobe(fnName, kretprobe, -1); err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to attach do_ret_sys_execve: %s\n", err)
 		os.Exit(1)
 	}
