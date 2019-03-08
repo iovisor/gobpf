3fdcd51d9b2df50a1aa7400b50e0a136b69bce14
Josh Chorlton <choo@stripe.com>
2019-03-08 15:29:23 -0800

    Rename bpf_common.h -> bcc_common.h
    See https://github.com/iovisor/bcc/commit/a9fdcf474db69a754ae460021f8428c1dac4191f

--- elf/include/bpf.h
+++ elf/include/bpf.h
@@ -9,7 +9,7 @@
 #define _UAPI__LINUX_BPF_H__
 
 #include <linux/types.h>
-#include <linux/bpf_common.h>
+#include <linux/bcc_common.h>
 
 /* Extended instruction set based on top of classic BPF */
 
